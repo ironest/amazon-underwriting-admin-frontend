ea8e148f3b58d410d8c57d28e0f2ac9cad60442d
Ricky <ironest@gmail.com>
2020-01-28 23:37:43 +1100

    Copied Navbar and Footer elements from public repo

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+// import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div className="nav-div" >
+                {/* <Link to="/">
+                    <button>HomePage</button>
+                </Link>
+                <Link to="/childcare">
+                    <button>Childcare</button>
+                </Link> */}
+            </div>
+        )
+    }
+}
+
+export default NavBar;
