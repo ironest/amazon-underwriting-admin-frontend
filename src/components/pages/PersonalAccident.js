edfcf1d26d836df5cfb8f03fc46d886a781cdd8d
Ricky <ironest@gmail.com>
2020-02-04 01:04:02 +1100

    Added custom pages for HBB and PersonalAccident

--- src/components/pages/PersonalAccident.js
+++ src/components/pages/PersonalAccident.js
@@ -1,13 +1,63 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPages } from "./../../actions";
+import Section from "./shared/Section";
 
 class PersonalAccident extends Component {
+
+    componentDidMount() {
+
+        this.props.fetchPages()
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     render() {
+
+        const {history} = this.props;
+
+        let [page] = this.props.pages.filter(page => {
+            return (page.name === "Personal Accident")
+        });
+
+        const currSections = page && page.sections;
+
+        [page] = this.props.pages.filter(page => {
+            return (page.name === "Common")
+        });
+
+        const commonSections = page && page.sections;
+
         return (
             <>
-                <h1>Personal Accident</h1>
+                <div className="row" >
+                    <div className="container">
+                        <h1>Personal Accident</h1>
+
+                        {
+                            currSections && currSections.map((section, idx) => {
+                                return <Section section={section} pageName="Childcare" key={`section-${idx}`} history={history}/>
+                            })
+                        }
+
+                        {
+                            commonSections && commonSections.map((section, idx) => {
+                                return <Section section={section} pageName="Childcare" key={`section-${idx}`} history={history}/>
+                            })
+                        }
+
+                    </div>
+                </div>
             </>
         );
     }
 }
 
-export default PersonalAccident;
+const mapStateToProps = (state) => {
+    return {
+        pages: state.pages
+    }
+}
+
+export default connect(mapStateToProps, { fetchPages })(PersonalAccident);
