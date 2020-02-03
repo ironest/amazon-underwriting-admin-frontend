d8ce341edd9c49bfc17790cc0f3dc62fb5defcfa
Ricky <ironest@gmail.com>
2020-02-04 01:02:37 +1100

    Added INFO reducer

--- src/reducers/info_reducer.js
+++ src/reducers/info_reducer.js
@@ -1,9 +1,9 @@
-const defaultState = [];
+const defaultState = {};
 
 export default (state = defaultState, action) => {
   switch(action.type) {
       case "GET_INFO":
-          return [...state, action.payload];
+          return action.payload;
       default:
           return state;
   }
