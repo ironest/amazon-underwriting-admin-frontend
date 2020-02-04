ee8897f651553d1309e7dd08df86934454c81e6b
Ricky <ironest@gmail.com>
2020-02-05 09:56:37 +1100

    Implemented GET_NEWS action and action-creator to dispatch newsletter

--- src/actions/index.js
+++ src/actions/index.js
@@ -93,3 +93,61 @@ export const setInfo = (data) => {
     });
   }
 }
+
+export const fetchNews = () => {
+  return async (dispatch, getState) => {
+      let response = await LocalApi.get("/news");
+      
+      return dispatch({
+          type: "GET_NEWS",
+          payload: response.data
+      });
+  }
+}
+
+export const setNews = (id, data) => {
+  return async (dispatch, getState) => {
+
+    let response = await LocalApi.put(`/news/${id}`, data)
+      .catch(err => {
+        console.log(err);
+      })
+    
+    return dispatch({
+        type: "GET_NEWS",
+        payload: response.data
+    });
+  }
+}
+
+export const deleteNews = (id) => {
+  console.log("deleteNews")
+  return async (dispatch, getState) => {
+    console.log("LocalApi.delete")
+    let response = await LocalApi.delete(`/news/${id}`)
+      .catch(err => {
+        console.log(err);
+      })
+    
+    return dispatch({
+        type: "GET_NEWS",
+        payload: response.data
+    });
+  }
+}
+
+export const createNews = (data) => {
+  console.log("createNews")
+  return async (dispatch, getState) => {
+    console.log("LocalApi.post")
+    let response = await LocalApi.post(`/news/`, data)
+      .catch(err => {
+        console.log(err);
+      })
+    
+    return dispatch({
+        type: "GET_NEWS",
+        payload: response.data
+    });
+  }
+}
