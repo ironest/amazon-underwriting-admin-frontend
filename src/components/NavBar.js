1a144869da813028a6b4f3e82f9b96b4c6e028f9
alkex1 <alikentwell@hotmail.com>
2020-02-05 22:35:02 +1100

    added Navbar + styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,27 @@ class NavBar extends Component {
     render() {
         return (
             <div className="nav-div" >
-                {/* <Link to="/">
-                    <button>HomePage</button>
+                <Link to="/">
+                    HomePage
                 </Link>
                 <Link to="/childcare">
-                    <button>Childcare</button>
-                </Link> */}
+                    Childcare
+                </Link>
+                <Link to="/homebasedbusinesses">
+                    Home Based Businesses
+                </Link>
+                <Link to="/personalaccident">
+                    Personal Accident
+                </Link>
+                <Link to="/fileupload">
+                    File Upload
+                </Link>
+                <Link to="/businessinfo">
+                    Businesses Info
+                </Link>
+                <Link to="/newsletter">
+                    Newsletter
+                </Link>
             </div>
         )
     }
